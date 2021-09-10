8c0cc8684e6c53e5df77af63d9350cacc1777a61
Rudie G <rauoldm@gmail.com>
2021-09-09 22:49:10 -0500

    added small seed to test database, server works now

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
-const path = require('path');
+// const path = require('path');
 const db = require('./config/connection')
 const cors = require('cors')
 const { ApolloServer } = require('apollo-server-express');
@@ -28,9 +28,9 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-    origin: `http://localhost:${PORT}`
-}))
+// app.use(cors({
+//     origin: `http://localhost:${PORT}`
+// }))
 
 // if (process.env.NODE_ENV === 'production') {
 //     app.use(express.static(path.join(__dirname, '../client/build')));
