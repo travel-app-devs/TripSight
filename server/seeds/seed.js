8420929217b4ce185308a33822942c0277a6e278
Rudie G <rauoldm@gmail.com>
2021-09-13 18:53:36 -0500

    album seeders

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,18 +1,21 @@
 const db = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Album } = require('../models');
 
-const userData = require('./userData.json');
-const postData = require('./postData.json');
+// const userData = require('./userData.json');
+// const postData = require('./postData.json');
+const albumData = require('./albumData.json');
 
 
 db.once('open', async () => {
   // clean database
-  await Post.deleteMany({});
-  await User.deleteMany({});
+  // await Post.deleteMany({});
+  // await User.deleteMany({});
+  await Album.deleteMany({});
 
   // bulk create each model
-  const users = await User.insertMany(userData);
-  const posts = await Post.insertMany(postData);
+  // const users = await User.insertMany(userData);
+  // const posts = await Post.insertMany(postData);
+  const albums = await Album.insertMany(albumData);
 
   console.log('all done!');
   process.exit(0);
