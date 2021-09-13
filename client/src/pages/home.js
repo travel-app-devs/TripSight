1c2ef3fb8f7aa5d9dd3b2cf3b76b3a17241ed79e
Kenneth Cheng <itskcheng@gmail.com>
2021-09-12 22:34:29 -0400

    Finished the homepage and finished the login and signup forms

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,8 +1,12 @@
-import Hero from '../component/hero'
+import Hero from '../component/hero';
+import Homepage from '../component/homepage';
+import Navigation from '../component/navigation';
 export default function Home() {
     return (
         <>
+            <Navigation />
             <Hero />
+            <Homepage />
         </>
     )
 }
