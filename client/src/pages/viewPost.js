f28b5555f99903fd2f13d0c361d5846410912b88
Kenneth Cheng <itskcheng@gmail.com>
2021-09-16 20:04:51 -0400

    Updated ViewPost

--- client/src/pages/viewPost.js
+++ client/src/pages/viewPost.js
@@ -4,14 +4,19 @@ import { useQuery } from '@apollo/client';
 import {QUERY_POST} from '../utils/queries';
 
 const ViewPost = () => {
-    const { loading, data } = useQuery(QUERY_POST);
+    //const { loading, data } = useQuery(QUERY_POST);
+    const { loading, data } = useQuery(QUERY_POST, { variables: {_id:"613ec026f588aeebab07fbc8" }});
     console.log('query data', data)
+
+    /*
+    const code = "613ec026f588aeebab07fbc8";
+    const { _id: code } = useParams();
+    console.log(code);
+    */
 
     const userData = data?.Posts || [];
     console.log(userData);
 
-    const { userId: userParam } = useParams();
-    console.log(userParam);
 
     if (loading) {
         return <div>Loading....</div>;
@@ -19,11 +24,7 @@ const ViewPost = () => {
 
     return(
         <div>
-            {
-        userData.filter((data) => data._id === userParam)
-        .map((data) => (
-            <h2>Title: {data.title}</h2>
-        ))}
+        
       </div>
     )
 }
