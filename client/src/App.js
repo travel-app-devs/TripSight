7bed1a10235a252fb861a58cd8580f5416bde601
Rudie G <rauoldm@gmail.com>
2021-09-17 11:36:29 -0500

    readme swap

--- client/src/App.js
+++ client/src/App.js
@@ -79,7 +79,7 @@ function App() {
   useEffect(() => {
     if(!document.querySelector("#here")) {
     const googleMapScript = document.createElement("script");
-    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?libraries=places`;
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAy6d25XL0PViXcyr-Erl3Gtg7SXYB0jRg&libraries=places`;
     googleMapScript.async = true;
     googleMapScript.id = "here";
     window.document.body.appendChild(googleMapScript);
