e1658360c4b68b5e75129a08bede2275b71ab944
Rudie G <rauoldm@gmail.com>
2021-09-17 18:30:43 -0500

    geolocation and searchfield put into form WIP

--- client/src/component/form/index.js
+++ client/src/component/form/index.js
@@ -1,11 +1,13 @@
 import style from './style.module.css'
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Auth from '../../utils/auth'
-
+import PlaceContext from '../../context/PlaceContext'
+import SearchField from '../searchField'
 import { useMutation } from '@apollo/client'
 import { ADD_POST } from '../../utils/mutations'
 
 export default function Form () {
+    const thePlace = useContext(PlaceContext)
     const [ titleInput, setTitleInput ] = useState('')
     const [ tagsInput, setTagsInput ] = useState('')
     const [ titleImageLink, setTitleLinksInput ] = useState('')
@@ -70,6 +72,26 @@ export default function Form () {
         }
     }
 
+    function geoFindMe(e) {
+        e.preventDefault()
+        function success(position) {
+          thePlace.setLatLng({
+              lat: position.coords.lat,
+              lng: position.coords.lng
+          })
+        }
+      
+        function error() {
+          alert('Unable to retrieve your location');
+        }
+      
+        if(!navigator.geolocation) {
+          alert('Geolocation is not supported by your browser');
+        } else {
+          navigator.geolocation.getCurrentPosition(success, error);
+        }
+      }
+
     const handleFormSubmit = async (event) => {
         event.preventDefault()
         const userId = Auth.getProfile().data._id
@@ -77,8 +99,8 @@ export default function Form () {
         let sepLinks = bodyImageLink.split(',')
         const tagsArray = sepTags.map(tag => tag.trim())
         const linksArray = sepLinks.map(link => link.trim())
-        // title: $title, titleImageLink: $titleImageLink, textBody: $textBody, bodyImageLinks: $bodyImageLinks, postVid: $postVid, latitude: $latitude, longitude: $longitude, description: $description, userId: $userId, pinned: $pinned, tags: $tags
-        setPost({title: titleInput, textBody: postInput, tags: tagsInput, titleImageLink: titleImageLink, bodyImageLinks: linksArray, postVid: postVidLink, userId: userId})
+        // title: $title, titleImageLink: $titleImageLink, textBody: $textBody, bodyImageLinks: $bodyImageLinks, postVid: $postVid, lat: $lat, lng: $lng, description: $description, userId: $userId, pinned: $pinned, tags: $tags
+        setPost({title: titleInput, textBody: postInput, tags: tagsInput, titleImageLink: titleImageLink, bodyImageLinks: linksArray, postVid: postVidLink, userId: userId, place: thePlace.place, lat: thePlace.latLng.lat, lng: thePlace.latLng.lng})
         try {
             const { data } = await addPost({
                 variables: {
@@ -86,9 +108,10 @@ export default function Form () {
                     titleImageLink: titleImageLink, 
                     textBody: postInput, 
                     bodyImageLinks: linksArray, 
-                    postVid: postVidLink, 
-                    latitude: 1, 
-                    longitude: 1, 
+                    postVid: postVidLink,
+                    place: thePlace.place,
+                    lat: thePlace.latLng.lat, 
+                    lng: thePlace.latLng.lng, 
                     description: null, 
                     userId: userId, 
                     pinned: false, 
@@ -141,6 +164,10 @@ export default function Form () {
                     <div className={style.postInput}>
                         <label htmlFor='post'>Post</label>
                         <textarea name='postInput' rows='5' cols='90' id='posts' onChange={handleChangePost} value={postInput} type='text'></textarea>
+                    </div>
+                    <SearchField />
+                    <div>
+                        <button onClick={(e) => geoFindMe(e)}>Click Here To Add Your Current Latitude and Longitude!</button>
                     </div>
                     
                 </form>
