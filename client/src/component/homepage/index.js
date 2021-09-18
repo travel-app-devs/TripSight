0aa20c35e2718b10cb3fa689ab154ff4ef64f685
Rudie G <rauoldm@gmail.com>
2021-09-17 22:12:53 -0500

    changed homepage words real quick

--- client/src/component/homepage/index.js
+++ client/src/component/homepage/index.js
@@ -6,16 +6,15 @@ const Homepage = () => {
   return (
     <div className={style.homepageContainer}>
       <div className={style.homepageSection}>
-        <h2 className={style.homepageHeader}>Find the best recommendation here</h2>
+        <h2 className={style.homepageHeader}>Find reviews, experiences, recommendations...</h2>
         <p className={style.homepageInformation}>
-          With Tripsight you can find recommendations from people that have
-          been there before you! You will know which places to visit while your
-          there and priotize your time.
+          With Tripsight you can find these and more from people that have
+          been to your next, or future, destination! Just search for your place of choice! If you want to post your own experiences, reviews, or recommendations, just sign up or log in. It's that easy!
         </p>
         <img id={style.blogWritingImage}src={world} />
       </div>
       <div className={style.homepageSectionTwo}>
-        <h2 className={style.homepageHeaderTwo}>Ready for your next adventure? Start looking at our collection of travel blogs today!</h2>
+        <h2 className={style.homepageHeaderTwo}>Ready for your next adventure? Get started at TripSight today!</h2>
       </div>
     </div>
   );
