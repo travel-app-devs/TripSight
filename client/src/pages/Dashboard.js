829caaccfdc736e5e2b85b24834a5a4ef37946c7
Rudie G <rauoldm@gmail.com>
2021-09-21 17:17:41 -0500

    mostly refactored

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,9 @@
-import React, { useContext, setContext } from 'react';
+import React, { useContext } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import Map from '../component/map';
-
+import UserContext from '../context/UserContext';
+import PostsContext from '../context/PostsContext';
 import DashInfo from '../component/dashInfo'
 
 import { QUERY_USERPOSTS, QUERY_USER } from '../utils/queries';
@@ -10,17 +11,16 @@ import { QUERY_USERPOSTS, QUERY_USER } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Dashboard = () => {
-  const userProfile = Auth.getProfile().data
-  console.log('userprofile', userProfile)
-  const { loading: userPostsLoading, data: userPosts } = useQuery(QUERY_USERPOSTS, { variables: {userId: userProfile._id }});
-  const { loading: userLoading , data: user } = useQuery(QUERY_USER, { variables: {_id: userProfile._id}});
+  const userDataContext = useContext(UserContext);
+  const postsDataContext = useContext(PostsContext);
+  userDataContext.setUserData(Auth.getProfile().data)
+  console.log('userprofile', userDataContext.userData)
+  postsDataContext.setUserPosts(useQuery(QUERY_USERPOSTS, { variables: {userId: userDataContext.userData._id }}));
+  userDataContext.setUserData(useQuery(QUERY_USER, { variables: {_id: userDataContext.userData._id}}));
 
   console.log('query data', user, userPosts)
 
-  if (userPostsLoading || userLoading) {
-    return <div>Loading....</div>;
-  }
-  if (!userProfile?.username) {
+ if (!userDataContext.userData?.username) {
     return (
       <h4>
         You need to be logged in to see this. Use the navigation links above to
@@ -31,8 +31,8 @@ const Dashboard = () => {
 
   return (
     <div>
-      <Map userPosts={userPosts} />
-      <DashInfo user={user.user} userPosts={userPosts.userPosts}/>
+      <Map userPosts={postsDataContext.userPosts} />
+      <DashInfo user={userDataContext.userData} userPosts={postsDataContext.userPosts}/>
 
       </div>
   );
